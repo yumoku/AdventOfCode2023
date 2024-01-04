afdbfe4a2454b0de4b6aff98d7df057ec09dd399
Yu Xiao <yxiao@cox.com>
2024-01-04 18:48:52 -0500

    Complete Day 2

--- Day1.py
+++ Day1.py
@@ -2,13 +2,28 @@
 import sys
 from pathlib import Path
 
-input_file = '/Users/yuxiao/Documents/Projects/exercise/AdventOfCode2023/data.txt'
+input_file = '/Users/yuxiao/PycharmProjects/AdventOfCode2023/Day1_data.txt'
 
 counter = 0
 container = []
 number = 0
+digits_in_letter = {'one': 1,
+                    'two': 2,
+                    'three': 3,
+                    'four': 4,
+                    'five': 5,
+                    'six': 6,
+                    'seven': 7,
+                    'eight': 8,
+                    'nine': 9}
 
-def count_digit(in_string):
+def combine_digits(in_string):
+    '''
+
+    :param: a string
+    :return: combine the first digit and the last digit into a number
+
+    '''
     container = []
     for index in range(0, len(in_string)):
         if in_string[index].isdigit():
@@ -18,12 +33,85 @@ def count_digit(in_string):
 
     return sum_digits
 
+def combine_digits_v2(in_string):
+    '''
+
+    :param in_string:
+    :return: combine the first digit and the last digit into a number including number in letter
+    '''
+
+    container = []
+    digits_info = {}
+    fist_and_last_digits = []
+    sum_digits = 0
+
+    digits_info = find_all_digits(in_string)
+    first_and_last_digits = find_first_and_last_digits(digits_info)
+    #print(first_and_last_digits)
+
+    sum_digits = int(str(first_and_last_digits[0]) + str(first_and_last_digits[-1]))
+
+    return sum_digits
+
+def find_all_digits(in_string):
+    '''
+
+    :param in_string:
+    :return: all the digits and their location
+    '''
+    digits_info = {}
+    indexes = []
+
+    for index in range(0, len(in_string)):
+        if in_string[index].isdigit():
+            digits_info[index] = int(in_string[index])
+
+    for key, value in digits_in_letter.items():
+        indexes = find_all_occurrences(in_string, key)
+
+        for item in indexes:
+            digits_info[item] = value
+
+    return digits_info
+
+def find_all_occurrences(main_string, sub_string):
+    '''
+
+    :param main_string:
+    :param sub_string:
+    :return: find all occurrence of the sub_string and return their indexes as list
+    '''
+    start = 0
+    indexes = []
+    while start < len(main_string):
+        index = main_string.find(sub_string, start)
+        if index == -1:
+            break
+        indexes.append(index)
+        start = index + 1
+    return indexes
+
+def find_first_and_last_digits(in_dict):
+    '''
+
+    :return: find the first and the last digit in a string in both digit or letter
+    '''
+    first_and_last_digits = []
+    indexes = []
+    first = 0
+    last = 0
+
+    for key in in_dict.keys():
+        indexes.append(int(key))
+
+    first = min(indexes)
+    last = max(indexes)
+    first_and_last_digits = [in_dict[first], in_dict[last]]
+    return first_and_last_digits
+
 with open(input_file, 'r') as input_file:
     for line in input_file:
-        number = 0
-
-        number = count_digit(line)
+        number = combine_digits_v2(line)
         counter += number
 
 print(counter)
-
--- Day2.py
+++ Day2.py
@@ -1,29 +1,93 @@
 
 import sys
 from pathlib import Path
+import re
 
-input_file = '/Users/yuxiao/Documents/Projects/exercise/AdventOfCode2023/data.txt'
+input_file = '/Users/yuxiao/PycharmProjects/AdventOfCode2023/Day2_data.txt'
 
+test_items = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+max_items = {}
 counter = 0
-container = []
-number = 0
+index = 0
+power = 0
 
-def count_digit(in_string):
-    container = []
-    for index in range(0, len(in_string)):
-        if in_string[index].isdigit():
-            container.append(int(in_string[index]))
+def convert_string_to_dict(in_string):
+    result_dict = {
+        "red": 0,
+        "blue": 0,
+        "green": 0,
+    }
+    in_list = []
 
-    sum_digits = int(str(container[0]) + str(container[-1]))
+    in_list = in_string.split(',')
 
-    return sum_digits
+    for item in in_list:
+        item = item.strip(' ').split(' ')
+
+        if "red" in item[1]:
+            result_dict["red"] = int(item[0])
+        if "blue" in item[1]:
+            result_dict["blue"] = int(item[0])
+        if "green" in item[1]:
+            result_dict["green"] = int(item[0])
+
+    return result_dict
+
+
+def calculate_max_items(in_string):
+    items = {}
+    max_items = {
+        "red": 0,
+        "blue": 0,
+        "green": 0
+    }
+    modified_string = []
+    each_line = ""
+
+    modified_string = in_string.split(':')[1].strip(' ').split(';')
+
+    for item in modified_string:
+        each_line = item.strip(' ')
+        items = convert_string_to_dict(each_line)
+        #print(items)
+
+        if items["red"] > max_items["red"]:
+            max_items["red"] = items["red"]
+        if items["blue"] > max_items["blue"]:
+            max_items["blue"] = items["blue"]
+        if items["green"] > max_items["green"]:
+            max_items["green"] = items["green"]
+
+    return max_items
+
+def compare_items(max_list, test_list):
+    flag = True
+
+    if max_list["red"] > test_list["red"]:
+        flag = False
+        #print("red flag is: ", flag)
+    if max_list["blue"] > test_list["blue"]:
+        flag = False
+        #print("blue flag is: ", flag)
+    if max_list["green"] > test_list["green"]:
+        flag = False
+        #print("green flag is: ", flag)
+    return flag
 
 with open(input_file, 'r') as input_file:
     for line in input_file:
-        number = 0
+        index += 1
+        max_items = calculate_max_items(line)
 
-        number = count_digit(line)
-        counter += number
+        if compare_items(max_items, test_items):
+            # print(f"Game {index} passes")
+            counter += index
 
-print(counter)
+        power += max_items["red"] * max_items["blue"] * max_items["green"]
 
+print("The output of first half of the game is: ", counter)
+print("The output of the second half of the game is: ", power)
